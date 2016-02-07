736f6e03cda9e8be2c91fab7716e8228ef72e0b8
Tomek Popis <tomek@melon-it.com>
2016-02-07 15:20:04 +0100

    Configure project

---
+++ MelonBaseModel/MelonBaseModel/MelonBaseModel.h
@@ -0,0 +1,19 @@
+//
+//  MelonBaseModel.h
+//  MelonBaseModel
+//
+//  Created by Tomasz Popis on 07/02/16.
+//  Copyright © 2016 Melon-IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MelonBaseModel.
+FOUNDATION_EXPORT double MelonBaseModelVersionNumber;
+
+//! Project version string for MelonBaseModel.
+FOUNDATION_EXPORT const unsigned char MelonBaseModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MelonBaseModel/PublicHeader.h>
+
+
