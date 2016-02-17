a7cd5a6c4d13dae242f73d05815260659288932f
Tomek Popis <tomek@melon-it.com>
2016-02-17 20:02:55 +0100

    Add functionality to detect the type of device

---
+++ MelonBaseModel/MelonBaseModel/MBFBaseManager.swift
@@ -0,0 +1,45 @@
+//
+//  MBFBaseManager.swift
+//  MelonBaseModel
+//
+//  Created by Tomasz Popis on 17/02/16.
+//  Copyright © 2016 Melon-IT. All rights reserved.
+//
+
+import Foundation
+
+public class MBFBaseManager {
+  
+  public init() {}
+  
+  public static var isIPhone: Bool {
+    
+    return (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone)
+  }
+  
+  public static var isIPad: Bool {
+    
+    return (UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad)
+  }
+  
+  public static var isIPhone3_5Inch: Bool {
+    
+    return (UIScreen.mainScreen().bounds.size.height == 480)
+  }
+  
+  public static var isIPhone4Inch: Bool {
+    
+    return (UIScreen.mainScreen().bounds.size.height == 568)
+  }
+  
+  public static var isIPhone4_7Inch: Bool {
+    
+    return (UIScreen.mainScreen().bounds.size.height == 667)
+  }
+  
+  public static var isIPhone5_5Inch: Bool {
+    
+    return (UIScreen.mainScreen().scale == 3)
+  }
+  
+}
