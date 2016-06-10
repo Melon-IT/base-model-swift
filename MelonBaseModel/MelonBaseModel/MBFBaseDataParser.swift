83e774ca5cd18cd367aa56d7ad74ea316f250ff3
Tomek Popis <tomek@melon-it.com>
2016-06-10 13:33:43 +0200

    Add the MBFBaseDataParser class.

---
+++ MelonBaseModel/MelonBaseModel/MBFBaseDataParser.swift
@@ -0,0 +1,13 @@
+//
+//  MBFBaseDataParser.swift
+//  MelonBaseModel
+//
+//  Created by Tomasz Popis on 10/06/16.
+//  Copyright © 2016 Melon-IT. All rights reserved.
+//
+
+import Foundation
+
+public class MBFBaseDataParser {
+  
+}
