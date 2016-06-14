de55a9f432d2bc440ac82ce595f8cabe439c0d57
Tomek Popis <tomek@melon-it.com>
2016-06-14 13:32:05 +0200

    Add public interface for the MBFBaseDataParser.
    - implements data access behaviour based on the NSUserDefaults

--- MelonBaseModel/MelonBaseModel/MBFBaseDataParser.swift
+++ MelonBaseModel/MelonBaseModel/MBFBaseDataParser.swift
@@ -10,4 +10,36 @@ import Foundation
 
 public class MBFBaseDataParser {
   
+  public func loadData() {
+    
+  }
+  
+  public func saveData() {
+    
+  }
+  
+  public func parseData(data: AnyObject?) {
+    
+  }
+  
+  public func loadFromDefaults(key: String) -> AnyObject? {
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    return defaults.objectForKey(key)
+  }
+  
+  public func saveToDefaults(key: String, object: AnyObject?) {
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    defaults.setObject(object, forKey: key)
+    defaults.synchronize()
+  }
+  
+  public func uniqueUserKey(userId: String,
+                            separator: String,
+                            suffix: String) -> String {
+    
+    return "\(userId)\(separator)\(suffix)"
+  }
+  
 }
